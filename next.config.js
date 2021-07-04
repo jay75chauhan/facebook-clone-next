c556d84e6167559b5f23a4a03339271d4946a157
jay75chauhan <chauhan75jayk@gmail.com>
2021-07-04 16:02:41 +0530

    google login add

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ module.exports = {
       "links.papareact.com",
       "platform-lookaside.fbsbx.com",
       "firebasestorage.googleapis.com",
+      "accounts.google.com",
+      "lh3.googleusercontent.com",
     ],
   },
 };
