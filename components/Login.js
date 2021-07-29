07ecb5665fb9fb9783720b24f33df72d7f0eaef9
jay75chauhan <chauhan75jayk@gmail.com>
2021-07-29 15:56:27 +0530

    login addit

--- components/Login.js
+++ components/Login.js
@@ -12,7 +12,7 @@ function Login() {
         <link rel="icon" href="https://links.papareact.com/5me" />
       </Head>
 
-      <div className="bg-gray-200  shadow-2xl  rounded-3xl items-center justify-center p-12 sm:p-24  ">
+      <div className="bg-gray-500  drop-shadow-2xl  rounded-2xl items-center justify-center p-12 sm:p-24  ">
         <div className="animate-bounce flex  items-center justify-center">
           <Image
             src="https://links.papareact.com/5me"
@@ -23,7 +23,7 @@ function Login() {
         </div>
         <h1
           onClick={signIn}
-          className="p-3 text-xl hover:text-gray-900 transition duration-200 transform ease-in hover:scale-105 hover:shadow-md mt-8  bg-blue-500  rounded-full text-white text-center cursor-pointer"
+          className="p-3 text-xl font-bold hover:text-gray-900 transition duration-200 transform ease-in hover:scale-105 hover:shadow-md mt-8  bg-blue-500  rounded-full text-white text-center cursor-pointer"
         >
           Login{" "}
         </h1>
