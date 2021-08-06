24fca25e09a77061ddb1ab649ed163b0273ec0e2
jay75chauhan <chauhan75jayk@gmail.com>
2021-08-06 21:08:59 +0530

    login page edited

--- components/Login.js
+++ components/Login.js
@@ -5,15 +5,22 @@ import Image from "next/image";
 
 function Login() {
   return (
-    <div className=" flex h-screen bg  bg-gradient-to-br from-gray-700 to-gray-200 items-center justify-center">
+    <div className="  flex h-screen items-center justify-center">
       <Head>
-        <title>Login Account</title>
+        <title>Facebook Login </title>
         <meta name="description" content="login " />
         <link rel="icon" href="https://links.papareact.com/5me" />
       </Head>
+      <div className="  flex  items-center justify-center">
+        <Image
+          src="https://images.unsplash.com/photo-1611162618071-b39a2ec055fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
 
-      <div className="bg-gray-500  drop-shadow-2xl  rounded-2xl items-center justify-center p-12 sm:p-24  ">
-        <div className="animate-bounce flex  items-center justify-center">
+      <div className="bg-transparent   backdrop-blur-2xl z-20  shadow-2xl  rounded-3xl items-center justify-center p-12 sm:p-24  ">
+        <div className=" flex hover:animate-spin  items-center justify-center">
           <Image
             src="https://links.papareact.com/5me"
             height={150}
@@ -21,19 +28,21 @@ function Login() {
             object-fit="contain"
           />
         </div>
-        <h1
-          onClick={signIn}
-          className="p-3 text-xl font-bold hover:text-gray-900 transition duration-200 transform ease-in hover:scale-105 hover:shadow-md mt-8  bg-blue-500  rounded-full text-white text-center cursor-pointer"
-        >
-          Login{" "}
-        </h1>
-        <a
-          class=" mt-4  pl-32 text-gray-400 hover:text-gray-500 hover:underline no-underline md:hover:underline text-xs"
-          href="https://github.com/jay75chauhan"
-          target="_blank"
-        >
-          @jayChauhan
-        </a>
+        <div className="flex flex-col   mt-12">
+          <button
+            onClick={signIn}
+            className=" py-3 backdrop-blur-3xl  text-xl  bg-transparent border-2 border-gray-300 shadow-inner  font-medium hover:text-gray-100 transition  transform duration-500 ease-in-out hover:scale-105 hover:shadow-2xl active:bg-blue-500    rounded-full text-gray-300 text-center cursor-pointer"
+          >
+            Login with{" "}
+          </button>
+          <a
+            className=" mt-4  pl-32 text-gray-200 hover:text-gray-300 hover:underline no-underline md:hover:underline text-xs"
+            href="https://github.com/jay75chauhan"
+            target="_blank"
+          >
+            @jayChauhan
+          </a>
+        </div>
       </div>
     </div>
   );
